65f38e28f2fa2692e4d42faa5aba305bca0ad6b9
Kim, Jungwon <kimj@ornl.gov>
2021-11-29 19:20:19 -0500

    removed docs

--- docs/html/iris.js
+++
@@ -1,9 +0,0 @@
-function copytoclipboard(id) {
-  var r = document.createRange();
-  r.selectNode(document.getElementById(id));
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(r);
-  document.execCommand('copy');
-  window.getSelection().removeAllRanges();
-}
-
