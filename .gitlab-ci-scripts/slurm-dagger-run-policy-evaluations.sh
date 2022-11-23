a4dcfc293548f0197c541fc74b7586eab17625a3
Aaron Young <youngar@ornl.gov>
2022-11-23 12:25:38 -0500

    Removed generated files. Fixed artifact collection.

--- .gitlab-ci-scripts/slurm-dagger-run-policy-evaluations.sh
+++ .gitlab-ci-scripts/slurm-dagger-run-policy-evaluations.sh
@@ -9,8 +9,6 @@
 #SBATCH --partition=gpu
 #SBATCH --nodelist=zenith
 
-set -x
-
 GIT_ROOT=$(git rev-parse --show-toplevel)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -20,6 +18,7 @@ echo Path: $GIT_ROOT
 echo $SHELL
 echo --------------------------------------------------------------------------------
 
+# Setup Environment
 cd $GIT_ROOT
 unset MODULEPATH_ROOT
 unset MODULEPATH
@@ -29,16 +28,17 @@ source $CONDA_ROOT/etc/profile.d/conda.sh
 source /auto/software/iris/setup_system.source
 source $IRIS_INSTALL_ROOT/setup.source
 # source /auto/ciscratch/conda/etc/profile.d/conda.sh
-cd apps/dagger
-which conda
+pushd apps/dagger
 conda env create --force -p ./envs -f dagger.yaml
 conda activate ./envs
-./run-policy-evaluation.sh
 
+# Run command
+./run-policy-evaluation.sh
+popd
+
+# Collect Output
 grep -e '[E]' slurm-test-out.txt > errors.txt
 
 echo --------------------------------------------------------------------------------
 echo Finished: $(date)
 echo Errors: $(wc -l errors.txt)
-echo Results: 
-cat *.csv
