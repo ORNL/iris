444324c299a3d84dcff9909a2c78529c2036ea05
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2023-01-11 12:42:58 -0500

    Added a condition to release object

--- src/runtime/Retainable.h
+++ src/runtime/Retainable.h
@@ -18,7 +18,11 @@ public:
     uid_ = iris_create_new_uid();
     struct_obj_.class_obj = (class_type*) this;
     ref_cnt_ = 1;
+    is_release_ = true;
   }
+  bool IsRelease() { return is_release_; }
+  void DisableRelease() { is_release_ = false; }
+  void EnableRelease() { is_release_ = true; }
   virtual ~Retainable() {}
 
   unsigned long uid() { return uid_; }
@@ -34,12 +38,13 @@ public:
     int i;
     do i = ref_cnt_;
     while (!__sync_bool_compare_and_swap(&ref_cnt_, i, i - 1));
-    if (i == 1) delete this;
+    if (i == 1 && is_release_) delete this;
   }
 
 private:
   unsigned long uid_;
   int ref_cnt_;
+  bool is_release_;
   struct_type struct_obj_;
 };
 
