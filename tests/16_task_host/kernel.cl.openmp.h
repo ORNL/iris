1f308d33da36f377adee1b68bedd627e810e7206
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2023-01-25 11:14:04 -0500

    Fixed tests for justify

--- tests/16_task_host/kernel.cl.openmp.h
+++ tests/16_task_host/kernel.cl.openmp.h
@@ -3,7 +3,7 @@
 static void process(int* A, int* factor, IRIS_OPENMP_KERNEL_ARGS) {
   size_t _id;
 #pragma omp parallel for shared(A, factor) private(_id)
-  IRIS_OPENMP_KERNEL_BEGIN
+  IRIS_OPENMP_KERNEL_BEGIN(_id)
   A[_id] = _id * factor[0];
   IRIS_OPENMP_KERNEL_END
 }
