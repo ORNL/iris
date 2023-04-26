b3bc243e9ac9d8d74e50978a5156ad94e7f284aa
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2023-04-26 10:12:55 -0400

    Provided a fix to read xclbin (Xilinx FPGA) from any working directory

--- src/runtime/Platform.cpp
+++ src/runtime/Platform.cpp
@@ -251,6 +251,7 @@ int Platform::EnvironmentInit() {
   EnvironmentSet("KERNEL_SRC_CUDA",     "kernel.cu",          false);
   EnvironmentSet("KERNEL_BIN_CUDA",     "kernel.ptx",         false);
   EnvironmentSet("KERNEL_XILINX_XCLBIN", "kernel.xilinx.xclbin",  false);
+  EnvironmentSet("KERNEL_FPGA_XCLBIN",   "kernel-fpga.xclbin",  false);
   EnvironmentSet("KERNEL_INTEL_AOCX", "kernel.intel.aocx",  false);
   EnvironmentSet("KERNEL_SRC_HEXAGON",  "kernel.hexagon.cpp", false);
   EnvironmentSet("KERNEL_BIN_HEXAGON",  "kernel.hexagon.so",  false);
