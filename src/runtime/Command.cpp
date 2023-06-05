fd6b3ce67c78ba129a8f678e00587b7ad499cf2e
imo <monilm@ornl.gov>
2023-06-05 17:02:08 -0400

    made separate function for auto dag and auto flash logic

--- src/runtime/Command.cpp
+++ src/runtime/Command.cpp
@@ -138,41 +138,10 @@ Command* Command::CreateKernel(Task* task, Kernel* kernel, int dim, size_t* off,
   return cmd;
 }
 
-Command* Command::CreateKernel(Task* task, Kernel* kernel, int dim, size_t* off, size_t* gws, size_t* lws, int nparams, void** params, size_t* params_off, int* params_info, size_t* memranges) {
-  Command* cmd = Create(task, IRIS_CMD_KERNEL);
-  cmd->kernel_ = kernel;
-  cmd->dim_ = dim;
-  for (int i = 0; i < dim; i++) {
-    cmd->off_[i] = off ? off[i] : 0ULL;
-    cmd->gws_[i] = gws[i];
-    cmd->lws_[i] = lws ? lws[i] : 0ULL;
-  }
-  for (int i = dim; i < 3; i++) {
-    cmd->off_[i] = 0;
-    cmd->gws_[i] = 1;
-    cmd->lws_[i] = 1;
-  }
-  if (nparams > cmd->kernel_nargs_max_) {
-    delete[] cmd->kernel_args_;
-    cmd->kernel_args_ = new KernelArg[nparams];
-    cmd->kernel_nargs_max_ = nparams;
-  }
-  cmd->kernel_nargs_ = nparams;
-  KernelArg* args = cmd->kernel_args();
-  Task* task_prev = NULL;
-  for (int i = 0; i < nparams; i++) {
-    KernelArg* arg = args + i;
-    void* param = params[i];
-    int param_info = params_info[i];
-    if (param_info > 0) {
-      arg->mem = NULL;
-      arg->off = 0ULL;
-      arg->size = param_info;
-      if (param) memcpy(arg->value, param, arg->size);
-      continue;
-    }
-    size_t mem_off = 0ULL;
-    BaseMem* mem = cmd->platform_->GetMem((iris_mem) param);
+#ifdef AUTO_PAR
+void Command::create_dependency(Command* cmd, Task* task, 
+		int param_info, 
+		BaseMem* mem, Task* task_prev) {
 
 #ifdef AUTO_PAR
     if (param_info == iris_w || param_info == iris_rw) {
@@ -222,13 +191,12 @@ Command* Command::CreateKernel(Task* task, Kernel* kernel, int dim, size_t* off,
         	_error("Graph is NULL:%ld:%s\n", task->uid(), task->name());
 	    }
 	} else {
-	    printf("-------Flush task Null----------\n");
-
-    	    printf("Total dependency %d\n", task_flush->ndepends());
+	    //printf("-------Flush task Null----------\n");
+    	    //printf("Total dependency %d\n", task_flush->ndepends());
 	    task_flush->set_name(tn);
             task_flush->ReplaceDependFlushTask(task);	
             //task_flush->EraseDepend();
-	    printf("Total dependency %d\n",task_flush->ndepends());
+	    //printf("Total dependency %d\n",task_flush->ndepends());
 	}
 	task_flush->set_opt(task->get_opt());
 	task_flush->set_brs_policy(task->get_brs_policy());
@@ -257,21 +225,128 @@ Command* Command::CreateKernel(Task* task, Kernel* kernel, int dim, size_t* off,
     }
 #endif
 
+}
+#endif
+
+Command* Command::CreateKernel(Task* task, Kernel* kernel, int dim, size_t* off, size_t* gws, size_t* lws, int nparams, void** params, size_t* params_off, int* params_info, size_t* memranges) {
+  Command* cmd = Create(task, IRIS_CMD_KERNEL);
+  cmd->kernel_ = kernel;
+  cmd->dim_ = dim;
+  for (int i = 0; i < dim; i++) {
+    cmd->off_[i] = off ? off[i] : 0ULL;
+    cmd->gws_[i] = gws[i];
+    cmd->lws_[i] = lws ? lws[i] : 0ULL;
+  }
+  for (int i = dim; i < 3; i++) {
+    cmd->off_[i] = 0;
+    cmd->gws_[i] = 1;
+    cmd->lws_[i] = 1;
+  }
+  if (nparams > cmd->kernel_nargs_max_) {
+    delete[] cmd->kernel_args_;
+    cmd->kernel_args_ = new KernelArg[nparams];
+    cmd->kernel_nargs_max_ = nparams;
+  }
+  cmd->kernel_nargs_ = nparams;
+  KernelArg* args = cmd->kernel_args();
+  Task* task_prev = NULL;
+  for (int i = 0; i < nparams; i++) {
+    KernelArg* arg = args + i;
+    void* param = params[i];
+    int param_info = params_info[i];
+    if (param_info > 0) {
+      arg->mem = NULL;
+      arg->off = 0ULL;
+      arg->size = param_info;
+      if (param) memcpy(arg->value, param, arg->size);
+      continue;
+    }
+    size_t mem_off = 0ULL;
+    BaseMem* mem = cmd->platform_->GetMem((iris_mem) param);
+
+#ifdef AUTO_PAR
+    create_dependency(cmd, task, param_info, mem, task_prev);
+#endif
 /*
 #ifdef AUTO_PAR
-    if(mem->get_current_writing_task() != NULL 
-		    && mem->get_current_writing_task() != task){
-        task->AddDepend(mem->get_current_writing_task());	
-	//printf("Task Name %s\n", mem->get_current_writing_task()->name());
-    }
- 
     if (param_info == iris_w || param_info == iris_rw) {
+
+    	if(mem->get_current_writing_task() != NULL 
+		    && mem->get_read_task_list()->size() == 0 ){
+    	    if (mem->get_current_writing_task() != task) {
+            	    task->AddDepend(mem->get_current_writing_task());	
+	    }
+    	} else if (  mem->get_read_task_list()->size() != 0 ){
+  	    std::vector<Task*>* read_list =  mem->get_read_task_list();
+	    Task* read_task;
+            for( int i = 0 ; i < read_list->size(); i++){
+            //for( auto & read_task : read_list){
+            //for( auto & read_task : mem->get_read_task_list()){
+	        read_task = read_list->at(i);
+	    	if (read_task != task) {
+                	task->AddDepend(read_task);	
+	    	}
+	    }
+        }
+ 
 	//printf("Seting current task info %d\n", param_info);
+	task_prev = mem->get_current_writing_task();
  	mem->set_current_writing_task(task);
+ 	mem->erase_all_read_task_list();
   	task->add_to_write_list(mem);
+
+#ifdef AUTO_FLUSH
+    	//sprintf(tn, "%s-flush-out", mem->get_flush_task();
+	//cmd->platform_->TaskCreate(tn, false, mem->get_flush_task())
+	Task* task_flush = mem->get_flush_task();
+	Graph* graph_flush = cmd->platform_->get_current_graph();
+	//printf("-------Flush task Name: %s----------\n", task_flush->name());
+    	char tn[256];
+    	sprintf(tn, "%s-auto-flushed-out", task->name());
+	if ( task_flush == NULL){
+	    task_flush = Task::Create(cmd->platform_, IRIS_TASK, tn);
+            Command* cmd_flush = Command::CreateMemFlushOut(task_flush, (DataMem *) mem);
+            task_flush->AddCommand(cmd_flush);
+   	    mem->set_flush_task(task_flush);
+            task_flush->AddDepend(task);	
+	    if(graph_flush != NULL) {
+		    graph_flush->AddTask(task_flush);
+	    }else {
+		printf("-------Graph NULL----------\n");
+        	_error("Graph is NULL:%ld:%s\n", task->uid(), task->name());
+	    }
+	} else {
+	    //printf("-------Flush task Null----------\n");
+    	    //printf("Total dependency %d\n", task_flush->ndepends());
+	    task_flush->set_name(tn);
+            task_flush->ReplaceDependFlushTask(task);	
+            //task_flush->EraseDepend();
+	    //printf("Total dependency %d\n",task_flush->ndepends());
+	}
+	task_flush->set_opt(task->get_opt());
+	task_flush->set_brs_policy(task->get_brs_policy());
+	
+  	//platform_->GraphTask(get_graph(), mem->get_flush_task(), 1, NULL);
+        //platform_->TaskMemFlushOut(mem->get_flush_task(), mem) {
+        //task->AddDepend(mem->get_flush_task());	
+    	//iris_graph_task( graph, mem->get_flush_task(), 1, NULL );
+  	//platform_->GraphTask(get_graph(), mem->get_flush_task(), 1, NULL);
+    	//mem->get_flush_task(
+#endif
+
     }
     if (param_info == iris_r) {
+    	if(mem->get_current_writing_task() != NULL) {
+    	    if (mem->get_current_writing_task() != task) {
+            	    task->AddDepend(mem->get_current_writing_task());	
+	    }
+	}
+
   	task->add_to_read_list(mem);
+	if (mem->get_current_writing_task() != task) {
+  	    mem->add_to_read_task_list(task);
+  	    //set_current_writing_task(NULL);
+	}
     }
 #endif
 */
