baec7db4e52d1a2d4ff87e9efdc53a308fc41361
Beau Johnston <johnstonbe@ornl.gov>
2023-06-15 08:18:58 -0400

    fixed openmp version of the kernel

--- tests/22_json_mixed_args/kernel.openmp.c
+++ tests/22_json_mixed_args/kernel.openmp.c
@@ -1,20 +1,69 @@
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
-#include "iris/iris_openmp.h"
-#include "iris/gettime.h"
+#include <iris/iris_openmp.h>
 
-int saxpy(int32_t* Z, const int32_t* X, const int32_t* Y, int32_t SIZE, int32_t A, IRIS_OPENMP_KERNEL_ARGS)
-//int saxpy(int32_t* Z, int32_t A, const int32_t* X, const int32_t* Y, int32_t SIZE, int32_t *dspUsec, int32_t *dspCyc, IRIS_OPENMP_KERNEL_ARGS) 
-{
-  size_t i;
-  //printf("Kernel Launch parameters: X:%p Y:%p Z:%p A:%d xSize:%d dspUsec:%p dspCyc:%p\n", X, Y, Z, A, SIZE, dspUsec, dspCyc);
-  //IRIS_OPENMP_KERNEL_BEGIN(i)
-  printf("A:%d SIZE:%d\n", A, SIZE);
-  #pragma omp parallel for shared(Z, A, X, Y) private(i)
-  for(i=0; i<SIZE; i++)
-      Z[i] = A * X[i] + Y[i];
-  //IRIS_OPENMP_KERNEL_END
-  //printf("Kernel Launch parameters: X:%p Y:%p Z:%p A:%d xSize:%d dspUsec:%p dspCyc:%p\n", X, Y, Z, A, SIZE, dspUsec, dspCyc);
-  return 0;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  __global int * Z;
+  __global int * X;
+  __global int * Y;
+  int A;
+} iris_openmp_saxpy_args;
+iris_openmp_saxpy_args saxpy_args;
+
+static int iris_openmp_saxpy_setarg(int idx, size_t size, void* value) {
+  switch (idx) {
+    case 3: memcpy(&saxpy_args.A, value, size); break;
+    default: return IRIS_ERROR;
+  }
+  return IRIS_SUCCESS;
 }
+
+static int iris_openmp_saxpy_setmem(int idx, void* mem) {
+  switch (idx) {
+    case 0: saxpy_args.Z = (__global int*__restrict) mem; break;
+    case 1: saxpy_args.X = (__global int*__restrict) mem; break;
+    case 2: saxpy_args.Y = (__global int*__restrict) mem; break;
+    default: return IRIS_ERROR;
+  }
+  return IRIS_SUCCESS;
+}
+
+#include "kernel.openmp.h"
+
+int iris_openmp_kernel(const char* name) {
+  iris_openmp_lock();
+  if (strcmp(name, "saxpy") == 0) {
+    iris_openmp_kernel_idx = 0;
+    return IRIS_SUCCESS;
+  }
+  return IRIS_ERROR;
+}
+
+int iris_openmp_setarg(int idx, size_t size, void* value) {
+  switch (iris_openmp_kernel_idx) {
+    case 0: return iris_openmp_saxpy_setarg(idx, size, value);
+  }
+  return IRIS_ERROR;
+}
+
+int iris_openmp_setmem(int idx, void* mem) {
+  switch (iris_openmp_kernel_idx) {
+    case 0: return iris_openmp_saxpy_setmem(idx, mem);
+  }
+  return IRIS_ERROR;
+}
+
+int iris_openmp_launch(int dim, size_t off, size_t ndr) {
+  switch (iris_openmp_kernel_idx) {
+    case 0: saxpy(saxpy_args.Z, saxpy_args.X, saxpy_args.Y, saxpy_args.A, off, ndr); break;
+  }
+  iris_openmp_unlock();
+  return IRIS_SUCCESS;
+}
+
+#ifdef __cplusplus
+} /* end of extern "C" */
+#endif
+
