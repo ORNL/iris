0c099b827c1735959fbdf19cd204e683e5b02baf
Beau Johnston <johnstonbe@ornl.gov>
2023-06-26 11:24:46 -0400

    added charm-sycl compilation workflow

--- apps/benchmarking/memory-performance-codes/membench-charmsycl-profiling.cpp
+++ apps/benchmarking/memory-performance-codes/membench-charmsycl-profiling.cpp
@@ -47,8 +47,8 @@ int main(int argc, char** argv) {
   double t0, t1;
   int* A;
   const int dims = 1;
-  bool debug = false;
-  bool logging = false;
+  bool debug = true;
+  bool logging = true;
   bool add_work = true; //test that the correct work is occurring on the memory buffers---when this is false, the "nothing" kernel is run which instead is designed to just measure the memory transfer
 
   if (argc < 3) return 1;
@@ -75,6 +75,7 @@ int main(int argc, char** argv) {
     for (size_t j = 0; j < SIZE; j++) {
         A[j] = 0;
     }
+    {
     sycl::buffer<int, dims> A_dev(A, SIZE);
 
     if (logging) printf("Generating and issuing tasks...\n");
@@ -90,30 +91,33 @@ int main(int argc, char** argv) {
       if (add_work) {
         //__kernel void add_id(__global int* A) {
         auto ev = q.submit([&](sycl::handler& h) {
-            sycl::accessor<int, dims, sycl::access_mode::read_write> a(A_dev, h);
-            h.parallel_for(sycl::range<dims>{SIZE}, [=](sycl::id<dims> const& i) {
-                a[i] = a[i] + i;
+            sycl::accessor<int, dims, sycl::access_mode::read_write> d_a(A_dev, h);
+            int int_i = 0;
+            // basic kernel parallelism
+            h.parallel_for(A_dev.get_range(), [=](sycl::item<dims> i) {
+            //TODO: delete? h.parallel_for(sycl::range(1), [d_a, i = int_i + 1] (sycl::id<1> id) {
+            //work group parallelism
+            //h.parallel_for(sycl::range<1>(sycl::range<1>(SIZE), sycl::range<1>(1)), [=] (sycl::nd_item<1> i) {
+                d_a[i] = d_a[i] + (int)i;
             });
         });
         ev.wait();
-      } else {
-        //__kernel void nothing(__global int* A) {
-        auto ev = q.submit([&](sycl::handler& h) {
-            sycl::accessor<int, dims, sycl::access_mode::read_write> a(A_dev, h);
-            h.parallel_for(sycl::range<dims>{SIZE}, [=](sycl::id<dims> const& i) {
-                //
-            });
-        });
-        ev.wait();
+      //} else {
+      //  //__kernel void nothing(__global int* A) {
+      //  auto ev = q.submit([&](sycl::handler& h) {
+      //      sycl::accessor<int, dims, sycl::access_mode::read_write> a(A_dev, h);
+      //      h.parallel_for(sycl::range<dims>{SIZE}, [=](sycl::id<dims> const& i) {
+      //          //
+      //      });
+      //  });
+      //  ev.wait();
       }
     if (logging) printf("syncing...\n");
     //iris_synchronize();
     if (logging) printf("DONE.\n");
 
     t1 = now();
-    //err = iris_mem_release(mem_A);
-    assert(err != IRIS_ERROR && "ERROR freeing memory!");
-
+    }
     // verify the result
     int accelerator_sum = 0;
     for (int k = 0; k < SIZE; k++) {
@@ -133,4 +137,4 @@ int main(int argc, char** argv) {
   write(false, LOGFILE, 0.0, true); // and close the file with a new line
 
   return 0;
-}
+}}
