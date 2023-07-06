976402c4754ba0539288922e4492b39280afe487
Aaron Young <youngar@ornl.gov>
2023-07-06 13:53:36 -0400

    Updated dagger_runner.py based on dagger_runner.cpp.

--- apps/dagger/dagger_runner.py
+++ apps/dagger/dagger_runner.py
@@ -11,23 +11,110 @@ __version__ = "1.0"
 import iris
 import argparse
 import re
+import numpy as np
 import sys
 import dagger_generator as dg
 
+EXIT_FAILURE = 1
+
+scheduling_policy_lookup = {
+    "roundrobin": iris.iris_roundrobin,
+    "depend": iris.iris_depend,
+    "data": iris.iris_data,
+    "profile": iris.iris_profile,
+    "random": iris.iris_random,
+    "any": iris.iris_any,
+    "all": iris.iris_all,
+    "custom": iris.iris_custom,
+}
+
 def init_parser(parser):
-    pass
+    parser.add_argument("--size",required=True,type=int,help="The size of the memory buffers to use in this IRIS test.")
+    parser.add_argument("--repeats",required=True,type=int,help="The number of repeats.")
+    parser.add_argument("--logfile",required=True,type=str,help="The location to log the timing results.")
+    parser.add_argument("--scheduling-policy",default='roundrobin',help="all options include (roundrobin, depend, profile, random, any, all, custom) or any integer [0-9] denoting the device id to run all tasks on.")
+    parser.add_argument("--attach-debugger",action='store_true',help="Attach debugger on port 5678.")
+
+def run(args):
+    iris.init()
+    print(f"REPEATS:{args.repeats} LOGFILE:{args.logfile} POLICY:{args.scheduling_policy}")
+
+    for k in dg._kernels:
+        print(f"KERNEL: {k} available on {dg._concurrent_kernels[k]} devices concurrently")
+
+    for t in range(args.repeats):
+        num_buffers_used = 0
+        host_mem = []
+        buffer_type = []
+        dev_mem = []
+        sizecb = []
+
+        for kernel in dg._kernels:
+            for concurrent_device in range(dg._duplicates):
+                argument_index = 0
+                for buffer in dg._kernel_buffs[kernel]:
+                    # Create and add the host-side buffer based on it's type
+                    if buffer == 'r':
+                        # Create and populate memory
+                        tmp = np.arange(args.size**dg._dimensionality[kernel], dtype=np.double)
+                        for i in range(args.size**dg._dimensionality[kernel]):
+                            tmp[i] = i
+                        host_mem.append(tmp)
+                        num_buffers_used += 1
+                    elif buffer == 'w':
+                        tmp = np.arange(args.size**dg._dimensionality[kernel], dtype=np.double)
+                        host_mem.append(tmp)
+                        num_buffers_used += 1
+                    elif buffer == 'rw':
+                        # Create and populate memory
+                        tmp = np.arange(args.size**dg._dimensionality[kernel], dtype=np.double)
+                        for i in range(args.size**dg._dimensionality[kernel]):
+                            tmp[i] = i
+                        host_mem.append(tmp)
+                        num_buffers_used += 1
+                    else:
+                        print(f"\033[41mInvalid memory argument! Kernel {kernel} has a buffer of memory type {buffer} but only r,w or rw are allowed.\n\033[0m")
+                        exit(EXIT_FAILURE)
+                    buffer_type.append(buffer)
+                    iris_mem = iris.mem(host_mem[-1].nbytes)
+                    dev_mem.append(iris_mem)
+
+            sizecb.append(args.size**dg._dimensionality[kernel]*np.double(0).itemsize)
+            print(f"SIZE[{args.size}] MATRIX_SIZE[{sizecb[-1]/1024/1024}]MB")
+
+        json_inputs = []
+
+        json_inputs.append(args.size)
+        json_inputs.extend(sizecb)
+        json_inputs.extend(host_mem)
+        json_inputs.extend(dev_mem)
+        json_inputs.append(iris.iris_pending)
+        json_inputs.append(args.task_target)
+
+        print(json_inputs)
+
+        # TODO: Add iris run
+        # graph = iris.graph()
+        # graph.load('graph.json', json_inputs)
+
 
 def main(argv):
     # Parse the arguments
     args = dg.parse_args(additional_arguments=[init_parser])
 
+    if args.attach_debugger:
+        import debugpy
+        debugpy.listen(5678)
+        print("Waiting for debugger attach")
+        debugpy.wait_for_client()
+        # debugpy.breakpoint()
+
+    args.task_target = scheduling_policy_lookup[args.scheduling_policy]
+
     print(args)
     print (dg._graph,dg._depth,dg._num_tasks,dg._min_width,dg._max_width)
 
-    exit(0)
-    graph = iris.graph()
-    graph.load('graph.json', params)
-
+    run(args)
 
 if __name__ == '__main__':
     main(sys.argv)
