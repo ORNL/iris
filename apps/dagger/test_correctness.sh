b7146a81c559ed4a314f06e1e6a5cbfc619049fc
Beau Johnston <johnstonbe@ornl.gov>
2023-12-04 23:54:44 -0500

    modified the script to use the new sdq and ftf scheduling policies

--- apps/dagger/test_correctness.sh
+++ apps/dagger/test_correctness.sh
@@ -34,7 +34,7 @@ echo "*******************************************************************"
 [ $? -ne 0 ] && exit
 cat graph.json
 cp graph.json linear50-graph.json
-for POLICY in roundrobin depend profile random any all
+for POLICY in roundrobin depend profile random ftf sdq
 do
   echo "Running IRIS with Policy: $POLICY"
   IRIS_HISTORY=1 ./dagger_test --logfile="time.csv" --repeats=1 --scheduling-policy="$POLICY" --size=256  --kernels="bigk" --duplicates="0" --buffers-per-kernel="bigk:rw r r" --kernel-dimensions="bigk:2" --kernel-split='100' --depth=50 --num-tasks=50 --min-width=1 --max-width=1
