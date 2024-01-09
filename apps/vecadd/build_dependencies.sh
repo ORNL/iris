b67d2823b56204dbe5082c210b87826936a1c92e
Beau Johnston <johnstonbe@ornl.gov>
2024-01-09 00:29:09 -0500

    updated install script

--- apps/vecadd/build_dependencies.sh
+++ apps/vecadd/build_dependencies.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+WORKING_DIRECTORY=`pwd`
+
+#IRIS
+cd ../.. ; ./build.sh;
+cd $WORKING_DIRECTORY
 
 #Charm-SYCL
 git clone -b irisv3 git@code.ornl.gov:fujita/charm-sycl
 [ $? -ne 0 ] && exit
 cp build_charm_sycl.sh charm-sycl
 cd charm-sycl
-exec ./build_charm_sycl.sh
+./build_charm_sycl.sh
 [ $? -ne 0 ] && exit
 
-echo next one!
+cd $WORKING_DIRECTORY
+echo "todo dpc++ and adaptivecpp!"
