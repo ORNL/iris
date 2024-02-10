a67b621d4e70e3b2e54ce49692dbaa3a5472522a
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2024-02-10 12:36:54 -0500

    Fixed run policy with openmp extensions.

--- apps/dagger/kernel.openmp.h
+++ apps/dagger/kernel.openmp.h
@@ -24,6 +24,26 @@ extern void ijk(double* C, double* A, double* B, IRIS_OPENMP_KERNEL_ARGS) {
   IRIS_OPENMP_KERNEL_END
 }
 
+extern void special_ijk(double* C, double* A, double* B, IRIS_OPENMP_KERNEL_ARGS) {
+  size_t _id;
+#pragma omp parallel for shared(C, A, B) private(_id)
+  IRIS_OPENMP_KERNEL_BEGIN(_id)
+  size_t SIZE = _bws[0];
+  size_t j, k;
+  for (size_t j = 0; j < SIZE; j++) {
+    double sum = 0.0;
+    for (size_t k = 0; k < SIZE; k++) {
+      sum += A[_id * SIZE + k] * B[k * SIZE + j];
+    }
+    C[_id * SIZE + j] = sum;
+    int i=_id * SIZE + k;
+    double tmp = A[i];
+    A[i] = B[i];
+    B[i] = tmp;
+  }
+  IRIS_OPENMP_KERNEL_END
+}
+
 extern void bigk(double* C, double* A, double* B, IRIS_OPENMP_KERNEL_ARGS) {
   size_t _id;
 #pragma omp parallel for shared(C, A, B) private(_id)
