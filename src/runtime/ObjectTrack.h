06585a4c9f648999bc5bae25128069324e358866
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2024-04-20 19:56:53 -0400

    Simplified Device worker queue

--- src/runtime/ObjectTrack.h
+++ src/runtime/ObjectTrack.h
@@ -15,6 +15,7 @@ namespace iris {
                 ObjectTrack( ) {
                     //freed_objects = 0;
                     pthread_mutex_init(&track_lock_, NULL);
+                    _trace("Initialized track_lock:%p", track_lock_);
                 }
                 virtual ~ObjectTrack() {
                     pthread_mutex_destroy(&track_lock_);
@@ -32,40 +33,43 @@ namespace iris {
                 }
                 virtual bool CallBackIfObjectExists(unsigned long uid, SafeAssignmentCallBack callbackfn, void *lhs, void *rhs) {
                     bool object_exists = true;
-                    _debug2("Waiting for lock uid:%lu", uid);
+                    _trace("Waiting for lock uid:%lu track_lock:%p", uid, &track_lock_);
                     pthread_mutex_lock(&track_lock_);
-                    _debug2("Acquired lock uid:%lu", uid);
+                    _trace("Acquired lock uid:%lu track_lock:%p", uid, &track_lock_);
                     if (allocated_objects_.find(uid) != allocated_objects_.end() &&
                             allocated_objects_[uid] != NULL)
                         callbackfn(allocated_objects_[uid], lhs, rhs);
                     else
                         object_exists = false;
                     pthread_mutex_unlock(&track_lock_);
-                    _debug2("Released lock uid:%lu", uid);
+                    _trace("Released lock uid:%lu track_lock:%p", uid, &track_lock_);
                     return object_exists;
                 }
                 virtual bool CallBackIfObjectExists(unsigned long uid, ObjectTrackCallBack callbackfn) {
                     bool object_exists = true;
-                    _debug2("Waiting for lock uid:%lu", uid);
+                    _trace("Waiting for lock uid:%lu track_lock:%p", uid, &track_lock_);
                     pthread_mutex_lock(&track_lock_);
-                    _debug2("Acquired lock uid:%lu", uid);
+                    _trace("Acquired lock uid:%lu track_lock:%p", uid, &track_lock_);
                     if (allocated_objects_.find(uid) != allocated_objects_.end() &&
                             allocated_objects_[uid] != NULL)
                         callbackfn(allocated_objects_[uid]);
                     else
                         object_exists = false;
                     pthread_mutex_unlock(&track_lock_);
-                    _debug2("Released lock uid:%lu", uid);
+                    _trace("Released lock uid:%lu track_lock:%p", uid, &track_lock_);
                     return object_exists;
                 }
                 virtual void *GetObject(unsigned long uid) {
                     void *obj = NULL;
                     //_debug2("Waiting for lock uid:%lu", uid);
+                    _trace("Waiting for lock uid:%lu track_lock:%p", uid, &track_lock_);
                     pthread_mutex_lock(&track_lock_);
                     //_debug2("Acquired lock uid:%lu", uid);
+                    _trace("Acquired lock uid:%lu track_lock:%p", uid, &track_lock_);
                     if (allocated_objects_.find(uid) != allocated_objects_.end())
                         obj = allocated_objects_[uid];
                     pthread_mutex_unlock(&track_lock_);
+                    _trace("Released lock uid:%lu track_lock:%p", uid, &track_lock_);
                     //_debug2("Released lock uid:%lu", uid);
                     return obj;
                 }
@@ -81,26 +85,26 @@ namespace iris {
                     //freed_objects+=1; 
                 }
                 virtual void UntrackObject(void *p, unsigned long uid) {
-                    _debug2("Waiting for lock uid:%lu", uid);
+                    _trace("Waiting for lock uid:%lu track_lock:%p", uid, &track_lock_);
                     pthread_mutex_lock(&track_lock_);
-                    _debug2("Acquired lock uid:%lu", uid);
+                    _trace("Acquired lock uid:%lu track_lock:%p", uid, &track_lock_);
                     allocated_objects_[uid] = NULL;
-                    _trace("Untracking object: %lu: %p", uid, p);
+                    _trace("Untracking object: %lu: %p track_lock:%p", uid, p, &track_lock_);
                     //printf("Untracking object: %lu: %p\n", uid, p);
                     pthread_mutex_unlock(&track_lock_);
-                    _debug2("Released lock uid:%lu", uid);
+                    _trace("Released lock uid:%lu track_lock:%p", uid, &track_lock_);
                     //freed_objects+=1; 
                 }
                 virtual void TrackObject(void *p, unsigned long uid) {
-                    _debug2("Waiting for lock uid:%lu", uid);
+                    _trace("Waiting for lock uid:%lu track_lock:%p", uid, &track_lock_);
                     pthread_mutex_lock(&track_lock_);
-                    _debug2("Acquired lock uid:%lu", uid);
+                    _trace("Acquired lock uid:%lu track_lock:%p", uid, &track_lock_);
                     if (allocated_objects_.find(uid) != allocated_objects_.end()) 
                         allocated_objects_[uid] = p;
                     else
                         allocated_objects_.insert(pair<unsigned long, void *>(uid, p));
                     pthread_mutex_unlock(&track_lock_);
-                    _debug2("Released lock uid:%lu", uid);
+                    _trace("Released lock uid:%lu track_lock:%p", uid, &track_lock_);
 #if 0
                     if (freed_objects > 2048) {
                         Clear();
@@ -110,16 +114,16 @@ namespace iris {
                 }
                 virtual void Clear() {
                     //allocated_objects_.clear();
-                    _debug2("Waiting for lock");
+                    _trace("Waiting for lock");
                     pthread_mutex_lock(&track_lock_);
-                    _debug2("Acquired lock ");
+                    _trace("Acquired lock ");
                     for (auto i = allocated_objects_.begin(), 
                             last = allocated_objects_.end(); i != last; ) {
                         if ((*i).second == NULL) i = allocated_objects_.erase(i);
                         else ++i;
                     }
                     pthread_mutex_unlock(&track_lock_);
-                    _debug2("Released lock ");
+                    _trace("Released lock ");
                 }
                 void Print(const char *data="Task track") {
                     printf("%s\n", data);
@@ -133,8 +137,8 @@ namespace iris {
             private:
                 //int freed_objects;
                 std::map<unsigned long, void *> allocated_objects_;
-            protected:
                 pthread_mutex_t track_lock_;
+            protected:
         };
     }
 }
