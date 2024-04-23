1a2dd00d5812a7c12a7218baaace9dc75c2d62e6
Beau Johnston <johnstonbe@ornl.gov>
2024-04-23 15:00:37 -0400

    added kernel logging for all commands (including device initialization)

--- src/runtime/SchedulingHistory.cpp
+++ src/runtime/SchedulingHistory.cpp
@@ -39,10 +39,10 @@ SchedulingHistory::~SchedulingHistory() {
 
 void SchedulingHistory::AddKernel(Command* cmd) {
   //ignore logging kernels at the moment
-  if (cmd->type_init()){
-    printf("not logging initialization -- but performed init on device id %d\n",cmd->task()->dev()->devno());
-  }
-  //Add(cmd);
+  //if (cmd->type_init()){
+  //  printf("not logging initialization -- but performed init on device id %d\n",cmd->task()->dev()->devno());
+  //}
+  Add(cmd);
 }
 
 void SchedulingHistory::AddH2D(Command* cmd) {
