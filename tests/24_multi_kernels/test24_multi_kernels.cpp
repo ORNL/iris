b8a6962ebfba6db52ff5e9ee738f13de65d8c6e9
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2024-05-21 12:28:11 -0400

    Updated test

--- tests/24_multi_kernels/test24_multi_kernels.cpp
+++ tests/24_multi_kernels/test24_multi_kernels.cpp
@@ -28,14 +28,18 @@ int main(int argc, char** argv) {
 
   int ndevs;
   bool is_opencl_device=false;
+  bool is_cuda_device=false;
+  bool is_hip_device=false;
   iris_device_count(&ndevs);
   for (int d = 0; d < ndevs; d++){
     int backend_worker;
     iris_device_info(d, iris_backend, &backend_worker, nullptr);
     if (backend_worker == iris_opencl) is_opencl_device = true;
+    if (backend_worker == iris_cuda) is_cuda_device = true;
+    if (backend_worker == iris_hip) is_hip_device = true;
   }
-  if(!(is_opencl_device)){
-    printf("Skipping this test because it is only designed to test on OpenCL backends.\n");
+  if(!is_opencl_device && !is_cuda_device && !is_hip_device){
+    printf("Skipping this test because it is only designed to test on OpenCL or CUDA backends.\n");
     return 0;
   }
 
