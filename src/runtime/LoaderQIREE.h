3888422714875f0a64d772b55020b18cd13b2f82
imo <monilm@ornl.gov>
2025-01-15 10:34:11 -0500

    added the loader function for QIREE

---
+++ src/runtime/LoaderQIREE.h
@@ -0,0 +1,26 @@
+#ifndef IRIS_SRC_RT_LOADER_QIREE_H
+#define IRIS_SRC_RT_LOADER_QIREE_H
+
+#include "Loader.h"
+//#include <iris/qiree/qiree.h>
+
+namespace iris {
+namespace rt {
+
+class LoaderQIREE : public Loader {
+public:
+  LoaderQIREE();
+  ~LoaderQIREE();
+
+  //const char* library_precheck() { return "cuInit"; }
+  const char* library();// { return "libqiree.so"; }
+  int LoadFunctions();
+  void (*parse_input_c)(int, char **);
+
+};
+
+} /* namespace rt */
+} /* namespace iris */
+
+#endif /* IRIS_SRC_RT_LOADER_QIREE_H */
+
