0fdf6fea2339cacd958ad77e32b56f6bc910177c
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2025-02-11 21:04:58 -0500

    Added Saxpy JACC

---
+++ apps/saxpy/saxpy_jacc.jl
@@ -0,0 +1,54 @@
+const iris_path = ENV["IRIS"]
+ENV["LD_LIBRARY_PATH"] =  iris_path * "/lib64:" * iris_path * "/lib:" * ENV["LD_LIBRARY_PATH"]
+const iris_jl = iris_path * "/include/iris/IrisHRT.jl"
+include(iris_jl)
+
+using .IrisHRT
+using Base.Threads
+using KernelAbstractions
+
+# Opportunities
+## Can we identify in and outs automatically?
+## PIM specific intrinsics identification
+function saxpy(i, Z, A, X, Y)
+    if i <= length(Z)
+        @inbounds Z[i] = A*X[i] + Y[i]
+    end
+end
+
+function call(SIZE, A, X, Y, Z)
+    julia_start = time()
+    task0 = IrisHRT.parallel_for(SIZE, saxpy, Z, A, X, Y, flush=[Z])
+    julia_time = time() - julia_start
+    println("Julia time: ", julia_time)
+    return Z
+end
+m = parse(Int, ARGS[1])
+# Example usage
+
+SIZE = m
+function main(SIZE)
+    println("--------------")
+    #SIZE=8
+    A=2.0f0
+    IrisHRT.init()
+    gc_state = IrisHRT.gc_safe_enter()
+    X = IrisHRT.ones(Float32, SIZE)
+    Y = IrisHRT.ones(Float32, SIZE)
+    Z = IrisHRT.dmem(Float32, SIZE)
+
+    println("NDim:", IrisHRT.ndim(X))
+    println("Type:", IrisHRT.get_type(X))
+    println("Size:", IrisHRT.host_size(X))
+    #println("Data:", IrisHRT.valid_host(X))
+    call(SIZE, A, X, Y, Z)
+
+    Z_host = IrisHRT.host(Z)
+    println(Core.stdout, "Z    :", Z_host)
+    println(Core.stdout, "Z    :", Z_host[1:min(SIZE, 9)])
+
+    IrisHRT.gc_leave(gc_state)
+    IrisHRT.finalize()
+end
+
+main(SIZE)
