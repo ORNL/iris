b7665db5f2a7067227b2e5446f0cd9d4690f73ba
Narasinga Rao Miniskar <miniskarnr@ornl.gov>
2025-02-19 20:17:38 -0500

    Fixed a bug in QIREE

--- src/runtime/DeviceQIREE.cpp
+++ src/runtime/DeviceQIREE.cpp
@@ -219,7 +219,7 @@ int DeviceQIREE::KernelSetMem(Kernel* kernel, int idx, int kindex, BaseMem* mem,
 
 int DeviceQIREE::KernelLaunch(Kernel* kernel, int dim, size_t* off, size_t* gws, size_t* lws) {
   _trace("kernel[%s] dim[%d] off[%zu] gws[%zu]", kernel->name(), dim, off[0], gws[0]);
-
+  qiree_params_[0] = (void *)"null";
 #if 0
   char* argv[6]; 
   argv[0] = "null";
